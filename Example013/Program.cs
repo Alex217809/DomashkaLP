73fc7245aa8f15171209166001c44b19c9c45c66
Alex777 <alex21210978@gmail.com>
2023-07-16 20:47:16 +0300

    Сохранил файл1

--- Example013/Program.cs
+++ Example013/Program.cs
@@ -2,10 +2,10 @@
 {
     int number = (456);
     int f_digit = number / 100;
-    int s_digit = number % 100;
+    int s_digit = (number % 100)/10;
     int t_digit = number % 10;
 
-    Console.WriteLine($"Выводим второе число {s_digit/10}");
+    Console.WriteLine($"Выводим второе число {s_digit}");
 }
 
 Task9();
