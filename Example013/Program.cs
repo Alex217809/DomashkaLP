548d8a4132c27f40d2353bcda1ef9372f220ceb2
Alex777 <alex21210978@gmail.com>
2023-07-16 20:13:34 +0300

    Сделал задачу 1

---
+++ Example013/Program.cs
@@ -0,0 +1,11 @@
+﻿void Task9()
+{
+    int number = (456);
+    int f_digit = number / 100;
+    int s_digit = number % 100;
+    int t_digit = number % 10;
+
+    Console.WriteLine($"Выводим второе число {s_digit/10}");
+}
+
+Task9();
