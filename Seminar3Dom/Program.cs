d2d2ae3c089aa775bcb383087363c7b9a8d3ae85
Alex777 <alex21210978@gmail.com>
2023-07-22 01:03:25 +0300

    Задача 23

---
+++ Seminar3Dom/Program.cs
@@ -0,0 +1,26 @@
+﻿void Task23()
+{
+   //напищшите программу которая принимает на входе число N
+   // и выдает таблицу кубов чисел от 1 до Т
+   int input(string text)
+    {
+        Console.WriteLine(text);
+        return Convert.ToInt32(Console.ReadLine());
+    }
+   
+   int N = input("Введите число ");
+
+   Console.WriteLine ($"таблица кубов от 1 до {N} ");
+      
+      for (int i = 1; i <=N; i++)
+    {
+        {            
+            Console.WriteLine ($"{Math.Pow(i, 3)}");
+        }
+    }
+    Console.WriteLine();
+      
+}
+
+Console.Clear();
+Task23();
